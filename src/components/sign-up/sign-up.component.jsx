bd1582c92ec65c654bf55b80475ae7c97dc4b2d9
Brandon <88.brandoncr@gmail.com>
2019-11-05 16:36:19 +0100

    Added ability to store users in db through email sign up

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -55,40 +55,41 @@ export default class SignUp extends React.Component {
       <div className="sign-up">
         <h2 className="title">I don't have an account</h2>
         <span>Sign up with your email and password</span>
-        <form className="sign-up-form" onSubmit={this.handleSubmit}></form>
-        <FormInput
-          type="text"
-          name="displayName"
-          value={displayName}
-          onChange={this.handleChange}
-          label="Display Name"
-          required
-        ></FormInput>
-        <FormInput
-          type="email"
-          name="email"
-          value={email}
-          onChange={this.handleChange}
-          label="Email"
-          required
-        ></FormInput>
-        <FormInput
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-          label="Password"
-          required
-        ></FormInput>
-        <FormInput
-          type="password"
-          name="password"
-          value={password}
-          onChange={this.handleChange}
-          label="Confirm Password"
-          required
-        ></FormInput>
-        <CustomButton type="submit">Submit</CustomButton>
+        <form className="sign-up-form" onSubmit={this.handleSubmit}>
+          <FormInput
+            type="text"
+            name="displayName"
+            value={displayName}
+            onChange={this.handleChange}
+            label="Display Name"
+            required
+          ></FormInput>
+          <FormInput
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            label="Email"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            label="Password"
+            required
+          ></FormInput>
+          <FormInput
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={this.handleChange}
+            label="Confirm Password"
+            required
+          ></FormInput>
+          <CustomButton type="submit">Submit</CustomButton>
+        </form>
       </div>
     );
   }
